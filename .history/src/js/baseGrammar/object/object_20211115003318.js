945c30f5e217eb53a9e6e139b9aa4d53c578da4a
马世臣 <1934202608@qq.com>
2021-11-15 02:10:54 +0000

    删除文件 .history

--- .history/src/js/baseGrammar/object/object_20211115003318.js
+++
@@ -1,5 +0,0 @@
-console.log('hello world');
-
-log
-
-console.log(Object.getOwnPropertyDescriptors(Map));
--- .history/src/js/baseGrammar/object/object_20211115003402.js
+++
@@ -1,5 +0,0 @@
-console.log('hello world');
-console.log(233);
-
-console.log(Object.getOwnPropertyDescriptors(Map));
-
