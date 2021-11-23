c23cba50f504ce1ab248392692533aeed081de06
ma_shi_chen <1934202608@qq.com>
2021-11-23 15:40:13 +0800

    11.23

--- src/js/basicLearning/date.js
+++ src/js/basicLearning/date.js
@@ -1,4 +1,6 @@
     console.log(Date.parse('2011-10-10T14:48:00'));
 
     let d = new Date()
-    console.log(d.getHours());
+    // console.log(d.getHours());
+
+ 
--- src/js/electron/my-electron-app/main.js
+++ src/js/electron/my-electron-app/main.js
@@ -1,14 +1,28 @@
 const { app, BrowserWindow } = require('electron')
-
+const path = require('path')
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences:{
+            preload:path.join(__dirname,'./src/utils/preload.js')
+        }
     })
 
     win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-    createWindow()
+    createWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 })
