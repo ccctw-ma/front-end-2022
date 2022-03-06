acb7b71b41a5bf7800a6eeb64910489ad2c47666
= <1934202608@qq.com>
2022-03-06 13:28:39 +0800

    3.6

--- leetcode/Medium/optimalDivision.js
+++ leetcode/Medium/optimalDivision.js
@@ -1,7 +1,7 @@
 /**
  * @Author: msc
  * @Date: 2022-02-27 16:24:15
- * @LastEditTime: 2022-02-27 16:24:18
+ * @LastEditTime: 2022-03-01 23:43:56
  * @LastEditors: msc
  * @Description: 
  */
@@ -9,6 +9,18 @@
  * @param {number[]} nums
  * @return {string}
  */
-var optimalDivision = function(nums) {
-
+var optimalDivision = function (nums) {
+    let len = nums.length;
+    if (len === 1) return nums[0] + "";
+    if (len === 2) return nums[0] + "/" + nums[1];
+    const res = [];
+    res.push(nums[0]);
+    res.push("/(");
+    res.push(nums[1]);
+    for (let i = 2; i < n; i++) {
+        res.push("/");
+        res.push(nums[i]);
+    }
+    res.push(")");
+    return res.join('');
 };
--- src/js/basicLearning/promise.js
+++ src/js/basicLearning/promise.js
@@ -1,3 +1,10 @@
+/**
+ * @Author: msc
+ * @Date: 2021-11-16 23:58:53
+ * @LastEditTime: 2022-03-02 00:28:05
+ * @LastEditors: msc
+ * @Description: 
+ */
 // function timeout(ms) {
 //     return new Promise((resolve, reject) => {
 //         setTimeout(resolve, ms, 'done 2333');
@@ -43,56 +50,90 @@ allSettledPromise.then(function (results) {
 });*/
 
 
-const http = require('https');
-const resolve = require("@rollup/plugin-node-resolve");
+// const http = require('https');
 
-// 第三方天气接口
-let url = 'https://www.fastmock.site/mock/cbc7bb1f48d813d2e150862ce0b08c81/test/hello';
+// // 第三方天气接口
+// let url = 'https://www.fastmock.site/mock/cbc7bb1f48d813d2e150862ce0b08c81/test/hello';
 
-let req = http.request(url, function (res) {
-    let info = '';
+// let req = http.request(url, function (res) {
+//     let info = '';
 
-    res.on('data', function (chunk) {
-        info += chunk;
-    });
+//     res.on('data', function (chunk) {
+//         info += chunk;
+//     });
 
-    res.on('end', function (err) {
-        console.log(info);
-    });
-});
+//     res.on('end', function (err) {
+//         console.log(info);
+//     });
+// });
 
-req.end();
-
-
-
-
-const timeout = (ms) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(resolve, ms, new Array(10).fill(0).map((v, index) => index))
-    })
-}
-
-timeout(1000).then(value => {
-    console.log(value)
-})
-
-const promise = new Promise((resolve,reject)=>{
-    console.log("Promise");
-    resolve();
-})
-
-promise.then(()=>{
-    console.log("resolved")
-})
-
-console.log("hello world")
+// req.end();
 
 
 
 
+// const timeout = (ms) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(resolve, ms, new Array(10).fill(0).map((v, index) => index))
+//     })
+// }
+
+// timeout(1000).then(value => {
+//     console.log(value)
+// })
+
+// const promise = new Promise((resolve, reject) => {
+//     console.log("Promise");
+//     resolve();
+// })
+
+// promise.then(() => {
+//     console.log("resolved")
+// })
+
+// console.log("hello world")
 
 
 
+// const p1 = new Promise((resolve, reject) => {
+//     setTimeout(() => reject(new Error("fail")), 3000);
+// })
+
+// const p2 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve(p1), 1000)
+// })
+
+// p2.then(res => console.log(res)).catch(err => console.log(err))
+
+// const promise = new Promise((resolve, reject) => {
+//     reject(new Error("test"));
+// })
+// promise.catch(err => {
+//     console.log(err);
+// })
+
+// const someAsyncThing = () => {
+//     return new Promise((resolve, reject) => {
+//         resolve(x + 2);
+//     })
+// }
+// someAsyncThing().then(() => {
+//     console.log("everything is great");
+// })
+// setTimeout(() => { console.log(123); }, 3000)
+
+
+
+const p1 = new Promise((resolve, reject) => {
+    resolve("Hello")
+}).then(res => res)
+
+const p2 = new Promise((resolve, reject) => {
+    throw new Error("报错了");
+}).then(res => res)
+
+Promise.all([p1, p2]).then(res => console.log(res))
+    .catch(e => console.log(e))
 
 
 
